571df65c3940f85f6db07151b0d0be4e355ae71c
Vishesh-Shokeen <visheshshokeen1@gmail.com>
2024-02-22 18:43:20 +0530

    Config.

--- src/Store.js
+++ src/Store.js
@@ -6,12 +6,13 @@ const DB = {
         Work: {
         }
     },
-    activeBoard: 'Fitness'
+    activeBoard:''
 }
 
 const localStorageDB = localStorage.getItem('DB')
 if (!localStorageDB) {
     localStorage.setItem('DB', JSON.stringify(DB))
+    DB.activeBoard = Object.keys(DB['data'])[0]
 }
 const DB_TO_USE = localStorageDB ? JSON.parse(localStorageDB) : DB
 const store = writable(DB_TO_USE, () => {
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,11 +1,36 @@
 <script>
     import AddTaskDialog from "./AddTaskDialog.svelte";
     import logo from "/logo.svg";
+    import Dots from "/3dots.svg";
+    import bin from "/delete.svg";
+    import close from "/close.svg";
     import notelist from "/notelist.svg";
-    
+    import store from "../Store";
+
+    let editDialog;
+    $: activeBoard = $store.activeBoard;
     const showDialogFn = () => {
         document.querySelector("#addTaskDialog").showModal();
     };
+    const showEditDialogFn = () => {
+        editDialog.showModal();
+    };
+    const closeDialogFn = () => {
+        editDialog.close();
+    };
+
+    const deleteCurrentBoard = () => {
+        let boardList = Object.keys($store.data);
+        if (boardList.length > 1) {
+            delete $store.data[activeBoard];
+            let nextBoard = Object.keys($store.data)[0];
+            $store.activeBoard = nextBoard;
+        } else {
+            alert("Last Board cannot be deleted");
+        }
+        editDialog.close();
+    };
+
     const toggleNavFn = () => {
         const nav = document.querySelector("#navBar");
         !nav.getAttribute("showBar")
@@ -18,12 +43,32 @@
     <img on:click={toggleNavFn} src={notelist} alt="My Boards" />
     <img src={logo} alt="Logo" />
 
-    <button on:click={showDialogFn}>
-        +
-        <span>Add Task</span>
-    </button>
+    <div class="editWrapper">
+        <button on:click={showDialogFn}>
+            +
+            <span>Add Task</span>
+        </button>
+
+        <img on:click={showEditDialogFn} src={Dots} alt="Edit" />
+    </div>
 
     <AddTaskDialog />
+    <dialog bind:this={editDialog} id="editDialog">
+        <div>
+            <p>
+                <span>Delete {activeBoard}</span>
+                <span>This action cannot be undone.</span>
+            </p>
+            <div>
+                <img
+                    on:click={deleteCurrentBoard}
+                    src={bin}
+                    alt="Delete Board"
+                />
+                <img on:click={closeDialogFn} src={close} alt="Close" />
+            </div>
+        </div>
+    </dialog>
 </header>
 
 <style>
@@ -36,6 +81,19 @@
     img {
         cursor: pointer;
         width: 2rem;
+    }
+
+    .editWrapper {
+        display: inline-flex;
+    }
+
+    #editDialog div p span {
+        display: block;
+    }
+    #editDialog div > div {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
     }
 
     @media (width < 700px) {
